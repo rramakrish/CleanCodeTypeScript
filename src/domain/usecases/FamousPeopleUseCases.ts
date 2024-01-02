30e067a615cb7296417182c1f00fb4d38581f6d0
Raghu Ramakrishnan <raghuramakrishnan@Raghus-MacBook-Air.local>
2024-01-02 16:28:14 +0530

    some refactor
    Former-commit-id: 66ce7df601927569c25ea9de5033bf45f9cdbb93

--- src/domain/usecases/FamousPeopleUseCases.ts
+++ src/domain/usecases/FamousPeopleUseCases.ts
@@ -1,11 +1,13 @@
 import { Observable } from "rxjs";
 import { UseCase } from "../base/utils/UseCase";
 import { PeopleModel } from "../models/StarWarsPeople";
+import { IStarWars } from "../repositories/StarWars";
 
 export class FamousPeopleUseCases implements UseCase<{id:1},PeopleModel>{
+    constructor(private repository:IStarWars){}
     //this uses the repository to ultimately call the service
     execute(params: { id: 1; }): Observable<PeopleModel> {
-        throw new Error("Method not implemented.");
+        return this.repository.getPeople()
     }
 
 }
